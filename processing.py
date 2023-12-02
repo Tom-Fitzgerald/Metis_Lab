87e5fe564d33e44d3a4a0b63b1b8a37251156118
Tom-Fitzgerald <qng502@york.ac.uk>
2023-12-02 14:11:05 +0000

    Import data function created

--- processing.py
+++ processing.py
@@ -1,15 +1,37 @@
-#Importing Libraries
+# ---------- Importing Libraries ---------- #
+import os
 import scipy.io
 import numpy as np
 from matplotlib import pyplot as plt
 
-#Importing Datasets
-file_names = ["NBI(1e1)(1e1)","NBI(1e2)(1e2)","NBI(1e3)(1e3)","NBI(1e4)(1e4)","NBI(1e5)(1e5)","NBI(5e5)(5e5)","NBI(1e6)(1e6)","NBI(3e6)(3e6)","NBI(5e6)(5e6)","NBI(7e6)(7e6)","NBI(1e7)(1e7)"]
-dataset = []
-for i in range(len(file_names)):
-    dataset.append(scipy.io.loadmat("/Users/tomfitzgerald/Desktop/Python/Plasma Measurement and Data Analysis/Metis/Datasets/"+file_names[i]+".mat"))
+# ---------- Functions ---------- #
+def import_data(data_directory):
+    """Loads .mat data into a list in alphabetical order of file name
 
-#Functions
+    Args:
+        data_directory (string): holds the pathname of the file in which the .mat data is stored
+
+    Returns:
+        tuple: returns the list of imported datasets as an imutable list
+    """
+    #List all the files in the directory
+    file_list = os.listdir(data_directory)
+
+    #Filter files to include only .mat files
+    mat_files = [file for file in file_list if file.endswith('.mat')]
+
+    #Sort the list of files aphabetically
+    mat_files.sort()
+
+    #Loop through each .mat file and load its contents
+    mat_data = [] 
+    for i in range(len(mat_files)):
+        file_path = os.path.join(data_directory, mat_files[i])
+        mat_data.append(scipy.io.loadmat(file_path))
+
+    #Return a list containing all the datasets
+    return mat_data
+ 
 def list_subsections(dataset):
     print("subsections (I'm using zerod by default):")
     print(dataset['post'].dtype)
@@ -28,11 +50,15 @@ def get_average(dataset,start, end, index, subsection='zerod'):
     a = get_variable(dataset,index, subsection=subsection)
     return (np.mean(a[start:end]), np.std(a[start:end]))
 
+# ---------- Main Code ---------- #
 if __name__ == '__main__':
+
+    #Importing Datasets (Note that datasets are loaded in alphabetical order of file name)
+    dataset = import_data("/Users/tomfitzgerald/Desktop/Python/Plasma Measurement and Data Analysis/Metis/Datasets")
+
     average = []
-    values = ["ne0","te0","taue"]
     for i in range(len(dataset)):
-        mean,_ = get_average(dataset[i],30,100,"taue")
+        mean,_ = get_average(dataset[i],40,100,"taue")
         average.append(mean)
 
     #Plotting
@@ -40,8 +66,5 @@ if __name__ == '__main__':
     plt.plot([1e1,1e2,1e3,1e4,1e5,5e5,1e6,3e6,5e6,7e6,1e7],average,"x-")
     plt.xlabel(r"Neutral Beam Injection Power $(W)$") ; plt.ylabel(r"Energy Confinement Time $(s)$")
     plt.title(r"$\tau_{e}$ vs NBI Power")
-
-    plt.figure(2)
-    plt.plot(get_variable(dataset[4],'taue'))
 
     plt.show()
